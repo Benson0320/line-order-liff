46e4c169ebdedadbe205456f798846d977445dff
yuhsiu.cheng <yuhsiu.cheng@tpv-tech.com>
2026-08-01 12:39:55 +0800

    modify 網址

--- js/config.js
+++ js/config.js
@@ -1,12 +1,10 @@
 window.APP_CONFIG = Object.freeze({
   VERSION: "1.0.0-alpha.3",
 
-  // 請在正式部署前確認此 LIFF ID。
   LIFF_ID: "2010924806-TX9pddUE",
 
-  // Milestone 3 完成 Apps Script API 後，請替換成正式 Web App URL。
   WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbzot-sYDX-k3D1bLvrWn1New0oG_Grglomg0a9yonOKG8X0LT7pMWoOhOeS4yzx45wO/exec",
+    "https://script.google.com/macros/s/AKfycbwTmqIRLp9aqxuMVe6StBK7mFMFVumj45J_fKMwjEnCKI3r9VUXtLUerqfE7OI7S-KY/exec",
 
   API_ACTIONS: Object.freeze({
     PRODUCTS: "liffGetProducts",
@@ -24,6 +22,7 @@ window.APP_CONFIG = Object.freeze({
   MAX_QUANTITY: 9999,
   REQUEST_TIMEOUT_MS: 12000,
   SEND_TIMEOUT_MS: 12000,
+
   STORAGE_KEYS: Object.freeze({
     CART: "lineHairOrdering.cart.v1",
     RECENT_PRODUCTS: "lineHairOrdering.recentProducts.v1"
