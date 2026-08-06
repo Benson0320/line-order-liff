fe15faed87f695c624ccd38d9731aaef597b3b2d
Benson0320 <101245271+Benson0320@users.noreply.github.com>
2026-08-06 17:15:07 +0800

    Revert "feat: allow digits in customer name (supersedes D-010, see D-014)"
    This reverts commit d361119f14f3f8d8913c95a4dfc24a3c77eaa777.

--- js/utils.js
+++ js/utils.js
@@ -51,6 +51,13 @@
       };
     }
 
+    if (/[0-9０-９]/.test(customerName)) {
+      return {
+        valid: false,
+        message: "客戶名稱不可包含數字。"
+      };
+    }
+
     return {
       valid: true,
       value: customerName
